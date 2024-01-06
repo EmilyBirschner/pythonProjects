244068fec9acc217812d763165fa864fded0c72b
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-01-06 18:21:02 -0300

    Conversão de tipos, COERÇÃO

--- aula6.py
+++ aula6.py
@@ -5,6 +5,6 @@ tipos imutáveis e primitivos -> str, int, float, bool
 
 print('1',type('1')) #sem coerção
 print(int('1'),type(int('1'))) #coerção foi feita
-print(int('1') + 1)
-print(float('1.7') + 1)
-print(str(11)+'b')
+print(int('1') + 1) #coerção de string para int e soma
+print(float('1.7') + 1) #coerção de string para float e soma
+print(str(11)+'b') #coerção de int para string e concatenação
