bac7613f7c140c81406d4f3be3363f87defbeca3
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-01-06 17:50:15 -0300

    Tipo de dados INT e FLOAT

---
+++ aula4.py
@@ -0,0 +1,22 @@
+'''Tipos int e float
+int -> numero inteiro
+o tipo int representa qlqr número (+ ou -) sem sinal é +
+
+float-> numero com ponto flutuante
+o tipo float representa qlqr número (+ ou -) com ponto flutuante, sem sinal é +
+
+A classe(função) TYPE mostra o tipo que o Python inferiu ao valor
+'''
+
+print(11) #int
+print(-11) #int
+print(0) #int
+
+print(1.1, 10.11) #float
+print(-1.1, 0.0) #float
+
+print(type(1.9)) #vai mostrar o tipo do parâmetro que foi passado
+print(type("Uma string qlqr")) #vai mostrar o tipo do parâmetro que foi passado
+print(type("Uma string qlqr"), type(1), type(5.9)) #vai mostrar o tipo do parâmetro que foi passado
+
+
