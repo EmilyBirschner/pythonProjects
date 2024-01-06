682cb237d3b9025f4d6ef2da82f3a9c9035bcf7a
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-01-06 17:34:30 -0300

    Imprindo STRINGS

---
+++ aula3.py
@@ -0,0 +1,12 @@
+'''
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas (simples ou duplas)
+'''
+print(1234)
+print("Aspas duplas")
+print('Aspas simples')
+print('Caracter de \'escape\'')#menos utilizado
+print(r'Caracter de \'escape\'')#mais utilizado para funções regulares
+print('Caracter de "escape"')#mais utilizado
