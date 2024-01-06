7043c386e70865c33b0193dc1473dd00f8d4bbfd
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-01-06 17:15:27 -0300

    Usando a função PRINT

---
+++ aula2.py
@@ -0,0 +1,7 @@
+# Usando a função PRINT
+# \r\n -> CRLF
+# \n -> LF
+print(12, 34) #argumentos não nomeados
+print(12, 34, sep='-')# sep é um novo tipo de separador para argumentos não nomeados
+print(12, 34, sep='-', end='\n')# end é no fim do print quebrar a linha mas, já faz automaticamente
+print(12, 34, sep='-', end='\nquebra de linha')# end é no fim do print quebrar a linha mas, já faz automaticamente
