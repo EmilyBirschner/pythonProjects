cda90644d05bc2b8373dec08ba3ada3a545ee41b
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-01-06 18:00:30 -0300

    Tipo de dados BOOLEAN

---
+++ aula5.py
@@ -0,0 +1,7 @@
+'''Tipo de dado bool (boolean)-> (True ou False)
+Um operador pode ser o ==, que questiona se um valor é igual ao outro
+'''
+
+print(10 == 10) #True
+print(10 == 12) #False
+print(type(10 == 12)) #bool
