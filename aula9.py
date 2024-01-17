bb49018e2ed1f20d809f5b62452a520a70e7a572
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-01-17 18:03:57 -0300

    Operadores

--- aula9.py
+++ aula9.py
@@ -1,1 +1,22 @@
-#Operadores
+#Operadores Aritiméticos
+
+adicao = 10 + 10
+print('Adição: ', adicao)
+
+subtracao = 10 - 5
+print('Subtração: ', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação: ', multiplicacao)
+
+divisao = 10 / 2.2      
+print('Divisao: ', divisao)
+
+divisao_inteira = 10 // 2.2 
+print('Divisao inteira: ', divisao_inteira)
+
+exponencicao = 2 ** 10
+print('Exponenciação: ', exponencicao)
+
+modulo = 25 % 5
+print('Módulo: ', modulo)
