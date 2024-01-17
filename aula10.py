7776d9ed77483b0711876fb32fa77ee6781903fd
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-01-17 18:11:09 -0300

    Concatenação e repetição

---
+++ aula10.py
@@ -0,0 +1,9 @@
+#Concatenação e repetição
+
+concatenacao = 'A' + 'B' + 'C'
+print('Concateção: ',concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_luiz = 3 * 'Luiz'
+print('A dez vezes: ', a_dez_vezes)
+print('Três vezes Luiz: ', tres_vezes_luiz)
