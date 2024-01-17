d7bb6b9e2500946e672951ec0a04d686ad4d5a35
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-01-17 18:30:01 -0300

    Exercício - Cálculo do IMC

--- aula12.py
+++ aula12.py
@@ -3,8 +3,8 @@
 nome = 'Fulano de tal'
 altura = 1.80
 peso = 95
-imc = peso / (altura **2)
+imc = peso / (altura ** 2)
 
 print(...) #Ellipsis não gera erro no código
 
-print(nome, 'tem ', altura, 'de altura, pesa ', peso, 'kg e seu IMC é: ', imc)
+print(nome, 'tem', altura, 'de altura, pesa', peso, 'kg e seu IMC é:', imc)
