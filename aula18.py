a32adcaf8d91885a9d4d95734c142e4920ad0b10
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-01-18 17:45:47 -0300

    O Dubugger do VS Code e o interpretador do Python lendo os códigos

---
+++ aula18.py
@@ -0,0 +1,18 @@
+#O Dubugger do VS Code e o interpretador do Python lendo os códigos
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+    print('código para condição 1')
+elif condicao2:
+    print('código para condição 2')
+elif condicao3:
+    print('código para condição 3')
+elif condicao4:
+    print('código para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita')    
+    
