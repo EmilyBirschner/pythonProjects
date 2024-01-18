f974fc7dc61977d05a07ef5614e772cfbb6bf07e
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-01-18 17:34:47 -0300

    if / elif / else -> entendendo o fluxo do interpretador em condicionais

---
+++ aula17.py
@@ -0,0 +1,18 @@
+#if / elif / else -> entendendo o fluxo do interpretador em condicionais
+
+condicao1 = True
+condicao2 = False
+condicao3 = False
+condicao4 = False
+
+if condicao1:
+    print('código para condição 1')
+elif condicao2:
+    print('código para condição 2')
+elif condicao3:
+    print('código para condição 3')
+elif condicao4:
+    print('código para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita')    
+    
