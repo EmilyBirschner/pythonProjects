779e6681c1c61ff69faaee364c7cc7dab25c238c
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-01-23 16:18:04 -0300

    Fatiamento de strings

---
+++ aula27.py
@@ -0,0 +1,7 @@
+# Fatiamento de strings
+
+variavel = 'Olá mundo'
+print(variavel[4:7]) #início : fim (o fim pode ser omitido)
+print(len(variavel)) #conta a quantidade de caracteres
+print(variavel[0:9:2]) #passo
+print(variavel[::-1]) #inverte a string
