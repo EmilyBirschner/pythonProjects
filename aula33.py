219f6203c8b66cffabe5f44860c3a5909684488f
EmilyBirschner <emilyb.oliveira@hotmail.com>
2024-02-06 15:47:05 -0300

    Conversa - tipos built-in, documentação, tipos imutáveis, métodos de string

---
+++ aula33.py
@@ -0,0 +1,6 @@
+#Conversa - tipos built-in, documentação, tipos imutáveis, métodos de string
+#https://docs.python.org/pt-br/3/library/stdtypes.html
+
+string = 'Luiz Otávio'
+outra_variavel = f'{string[:3]}ABC{string[4:]}'
+print(outra_variavel)
